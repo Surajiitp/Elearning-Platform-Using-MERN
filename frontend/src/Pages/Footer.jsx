bb7de7c9ea02765bed220a2251e5ad86027b6ada
Suraj Kumar <razz912340@gmail.com>
2026-08-13 03:35:39 +0530

    Update frontend and footer

--- frontend/src/Pages/Footer.jsx
+++ frontend/src/Pages/Footer.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Grid, Heading, Link, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Heading,
+  Link,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 
 const Footer = () => {
   return (
@@ -17,13 +25,14 @@ const Footer = () => {
     >
       <Grid
         templateColumns={{
-          base: "repeat(1, 1fr)", // Smallest screen size (1 column)
+          base: "repeat(1, 1fr)",
           sm: "repeat(2, 1fr)",
-          md: "repeat(3, 1fr)", // Medium screen size (2 columns)
-          lg: "repeat(4, 1fr)", // Large screen size (4 columns)
+          md: "repeat(3, 1fr)",
+          lg: "repeat(4, 1fr)",
         }}
         gap={4}
       >
+        {/* Learn With SurajLearning */}
         <Box
           pl={{
             lg: "25px",
@@ -37,30 +46,33 @@ const Footer = () => {
           }}
         >
           <Heading as="h6" size="md" fontWeight="bold">
-            Learn Something New
+            Learn With SurajLearning
           </Heading>
+
           <br />
-          <Link fontSize="13.5px">Learn a Language</Link>
+
+          <Link fontSize="13.5px">Learn Web Development</Link>
           <br />
-          <Link fontSize="13.5px">Learn Accounting</Link>
+          <Link fontSize="13.5px">Learn JavaScript</Link>
           <br />
-          <Link fontSize="13.5px">Learn Coding</Link>
+          <Link fontSize="13.5px">Learn React</Link>
           <br />
-          <Link fontSize="13.5px">Learn Copywriting</Link>
+          <Link fontSize="13.5px">Learn Node.js</Link>
           <br />
-          <Link fontSize="13.5px">Learn HR</Link>
+          <Link fontSize="13.5px">Learn MongoDB</Link>
           <br />
-          <Link fontSize="13.5px">Learn Public Relations</Link>
+          <Link fontSize="13.5px">Learn Data Structures</Link>
           <br />
-          <Link fontSize="13.5px">Boulder MS Data Science</Link>
+          <Link fontSize="13.5px">Learn C++</Link>
           <br />
-          <Link fontSize="13.5px">Illinois iMBA</Link>
+          <Link fontSize="13.5px">Learn Python</Link>
           <br />
-          <Link fontSize="13.5px">Illinois MS Computer Science</Link>
+          <Link fontSize="13.5px">Learn SQL</Link>
           <br />
-          <Link fontSize="13.5px">UMich MS in Applied Data Science</Link>
+          <Link fontSize="13.5px">Learn Machine Learning</Link>
         </Box>
 
+        {/* Popular Topics */}
         <Box
           pl={{
             lg: "25px",
@@ -76,28 +88,29 @@ const Footer = () => {
           <Heading as="h6" size="md" fontWeight="bold">
             Popular Topics
           </Heading>
+
           <br />
-          <Link fontSize="13.5px">Accounting</Link>
+
+          <Link fontSize="13.5px">Web Development</Link>
           <br />
-          <Link fontSize="13.5px">Cybersecurity</Link>
+          <Link fontSize="13.5px">Data Structures & Algorithms</Link>
           <br />
-          <Link fontSize="13.5px">Data Analysis</Link>
+          <Link fontSize="13.5px">C++</Link>
           <br />
-          <Link fontSize="13.5px">Data Science</Link>
+          <Link fontSize="13.5px">JavaScript</Link>
           <br />
-          <Link fontSize="13.5px">Excel</Link>
+          <Link fontSize="13.5px">React</Link>
           <br />
-          <Link fontSize="13.5px">Google</Link>
+          <Link fontSize="13.5px">Node.js</Link>
           <br />
-          <Link fontSize="13.5px">Machine Learning</Link>
-          <br />
-          <Link fontSize="13.5px">Project Management</Link>
-          <br />
-          <Link fontSize="13.5px">Python</Link>
+          <Link fontSize="13.5px">MongoDB</Link>
           <br />
           <Link fontSize="13.5px">SQL</Link>
+          <br />
+          <Link fontSize="13.5px">Machine Learning</Link>
         </Box>
 
+        {/* Popular Courses */}
         <Box
           pl={{
             lg: "25px",
@@ -111,30 +124,29 @@ const Footer = () => {
           }}
         >
           <Heading as="h6" size="md" fontWeight="bold">
-            Popular Certificates
+            Popular Courses
           </Heading>
+
           <br />
-          <Link fontSize="13.5px">Google Data Analytics</Link>
+
+          <Link fontSize="13.5px">Complete Web Development</Link>
           <br />
-          <Link fontSize="13.5px">Google Digital Marketing & Ecommerce</Link>
+          <Link fontSize="13.5px">JavaScript Programming</Link>
           <br />
-          <Link fontSize="13.5px">Google IT Automation with Python</Link>
+          <Link fontSize="13.5px">React Development</Link>
           <br />
-          <Link fontSize="13.5px">Google IT Support</Link>
+          <Link fontSize="13.5px">MERN Stack Development</Link>
           <br />
-          <Link fontSize="13.5px">Google Project Management</Link>
+          <Link fontSize="13.5px">Data Structures & Algorithms</Link>
           <br />
-          <Link fontSize="13.5px">Google UX Design</Link>
+          <Link fontSize="13.5px">C++ Programming</Link>
           <br />
-          <Link fontSize="13.5px">IBM Data Analyst</Link>
+          <Link fontSize="13.5px">Database Management</Link>
           <br />
-          <Link fontSize="13.5px">IBM Data Science</Link>
-          <br />
-          <Link fontSize="13.5px">Intuit Bookkeeping</Link>
-          <br />
-          <Link fontSize="13.5px">Meta Front-End Developer</Link>
+          <Link fontSize="13.5px">Machine Learning</Link>
         </Box>
 
+        {/* SurajLearning */}
         <Box
           pl={{
             lg: "25px",
@@ -148,221 +160,26 @@ const Footer = () => {
           }}
         >
           <Heading as="h6" size="md" fontWeight="bold">
-            Featured Articles
+            SurajLearning
           </Heading>
-          <br />
-          <Link fontSize="13.5px">
-            A Comprehensive Guide to Becoming a Data Analyst
-          </Link>
-          <br />
-          <Link fontSize="13.5px">
-            Advance Your Career With A Cybersecurity Certification
-          </Link>
-          <br />
-          <Link fontSize="13.5px">Get Your Data Analytics Certification</Link>
-          <br />
-          <Link fontSize="13.5px">
-            How to Break into the Field of Data Analysis
-          </Link>
-          <br />
-          <Link fontSize="13.5px">
-            Jumpstart Your Data Career with a SQL Certification
-          </Link>
-          <br />
-          <Link fontSize="13.5px">Learn How to Become PMP Certified</Link>
-          <br />
-          <Link fontSize="13.5px">
-            Start Your Career with CAPM Certification
-          </Link>
-          <br />
-          <Link fontSize="13.5px">
-            Understanding the Role and Responsibilities of a Scrum Master
-          </Link>
-          <br />
-          <Link fontSize="13.5px">
-            Unlock Your Potential with a PMI Certification
-          </Link>
-          <br />
-          <Link fontSize="13.5px">
-            What You Should Know About CompTIA A+ Certification
-          </Link>
-        </Box>
 
-        <Box
-          pl={{
-            lg: "25px",
-            md: "15px",
-            sm: "10px",
-          }}
-          pr={{
-            lg: "35px",
-            md: "15px",
-            sm: "10px",
-          }}
-        >
-          <Heading as="h6" size="md" fontWeight="bold">
-            Coursera
-          </Heading>
           <br />
+
           <Link fontSize="13.5px">About</Link>
           <br />
-          <Link fontSize="13.5px">What We Offer</Link>
+          <Link fontSize="13.5px">Courses</Link>
           <br />
-          <Link fontSize="13.5px">Leadership</Link>
-          <br />
-          <Link fontSize="13.5px">Careers</Link>
-          <br />
-          <Link fontSize="13.5px">Catalog</Link>
-          <br />
-          <Link fontSize="13.5px">Coursera Plus</Link>
-          <br />
-          <Link fontSize="13.5px">Professional Certificates</Link>
-          <br />
-          <Link fontSize="13.5px">MasterTrack® Certificates</Link>
-          <br />
-          <Link fontSize="13.5px">Degrees</Link>
-          <br />
-          <Link fontSize="13.5px">For Enterprise</Link>
-          <br />
-          <Link fontSize="13.5px">For Government</Link>
-          <br />
-          <Link fontSize="13.5px">For Campus</Link>
-          <br />
-          <Link fontSize="13.5px">Become a Partner</Link>
-          <br />
-          <Link fontSize="13.5px">Coronavirus Response</Link>
-          <br />
-          <Link fontSize="13.5px">Free Courses</Link>
-          <br />
-          <Link fontSize="13.5px">All Courses</Link>
-        </Box>
-
-        <Box
-          pl={{
-            lg: "25px",
-            md: "15px",
-            sm: "10px",
-          }}
-          pr={{
-            lg: "35px",
-            md: "15px",
-            sm: "10px",
-          }}
-        >
-          <Heading as="h6" size="md" fontWeight="bold">
-            Community
-          </Heading>
-          <br />
-          <Link fontSize="13.5px">Learners</Link>
-          <br />
-          <Link fontSize="13.5px">Partners</Link>
-          <br />
-          <Link fontSize="13.5px">Beta Testers</Link>
-          <br />
-          <Link fontSize="13.5px">Translators</Link>
-          <br />
-          <Link fontSize="13.5px">Blog</Link>
-          <br />
-          <Link fontSize="13.5px">Tech Blog</Link>
-          <br />
-          <Link fontSize="13.5px">Teaching Center</Link>
-        </Box>
-
-        <Box
-          pl={{
-            lg: "25px",
-            md: "15px",
-            sm: "10px",
-          }}
-          pr={{
-            lg: "35px",
-            md: "15px",
-            sm: "10px",
-          }}
-        >
-          <Heading as="h6" size="md" fontWeight="bold">
-            More
-          </Heading>
-          <br />
-          <Link fontSize="13.5px">Press</Link>
-          <br />
-          <Link fontSize="13.5px">Investors</Link>
-          <br />
-          <Link fontSize="13.5px">Terms</Link>
-          <br />
-          <Link fontSize="13.5px">Privacy</Link>
-          <br />
-          <Link fontSize="13.5px">Help</Link>
-          <br />
-          <Link fontSize="13.5px">Accessibility</Link>
+          <Link fontSize="13.5px">Learning Resources</Link>
           <br />
           <Link fontSize="13.5px">Contact</Link>
           <br />
-          <Link fontSize="13.5px">Articles</Link>
+          <Link fontSize="13.5px">Privacy</Link>
           <br />
-          <Link fontSize="13.5px">Directory</Link>
-          <br />
-          <Link fontSize="13.5px">Affiliates</Link>
-          <br />
-          <Link fontSize="13.5px">Modern Slavery Statement</Link>
-        </Box>
-
-        <Box
-          pl={{
-            lg: "25px",
-            md: "15px",
-            sm: "10px",
-          }}
-          pr={{
-            lg: "35px",
-            md: "15px",
-            sm: "10px",
-          }}
-        >
-          <Flex
-            direction="column"
-            justifyContent="space-around"
-            alignContent="space-between"
-            h={{
-              lg: "100%",
-              sm: "100%",
-            }}
-          >
-            <Image
-              width={{
-                base:"35%",
-                sm: "20%",
-                md: "35%",
-                lg: "45%",
-              }}
-              src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d3njjcbhbojbot.cloudfront.net/web/images/icons/download_on_the_app_store_badge_en.svg?auto=format%2Ccompress&dpr=1&w=152&h=45&q=40"
-              alt="AppleStoreLogo"
-            />
-
-            <Image
-
-              width={{
-                base:"35%",
-                sm: "20%",
-                md: "35%",
-                lg: "45%",
-              }}
-              src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d3njjcbhbojbot.cloudfront.net/web/images/icons/en_generic_rgb_wo_45.png?auto=format%2Ccompress&dpr=1&w=152&h=45&q=40"
-              alt="Google_Play"
-            />
-            <Image
-              width={{
-                base:"15%",
-                sm: "15%",
-                md: "25%",
-                lg: "35%",
-              }}
-              src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d3njjcbhbojbot.cloudfront.net/web/images/icons/2018-B-Corp-Logo-Black-S.png?auto=format%2Ccompress&dpr=1&w=151&h=120&q=40"
-              alt="Certified"
-            />
-          </Flex>
+          <Link fontSize="13.5px">Terms</Link>
         </Box>
       </Grid>
+
+      {/* Bottom Footer */}
       <Flex
         mt={15}
         gap={7}
@@ -382,52 +199,39 @@ const Footer = () => {
           lg: "55px",
         }}
       >
+        {/* Copyright */}
         <Box>
-          {" "}
           <Text fontSize="13.5px">
-            © 2023 SRM Inc. All rights reserved.
+            © 2026 SurajLearning. All rights reserved.
           </Text>
         </Box>
-        <Flex spacing={4} overflow='hidden'>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/footer/facebook.png?auto=format%2Ccompress&dpr=1&w=28&h=28&q=40"
-            alt=""
-            mr={4}
-          />
 
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/footer/linkedin.png?auto=format%2Ccompress&dpr=1&w=28&h=28&q=40"
-            alt=""
-            mr={4}
-          />
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/footer/twitter.png?auto=format%2Ccompress&dpr=1&w=28&h=28&q=40"
-            alt=""
-            mr={4}
-          />
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/footer/youtube.png?auto=format%2Ccompress&dpr=1&w=28&h=28&q=40"
-            alt=""
-            mr={4}
-          />
-          <Image
-          display={{
-            base:"none",
-            sm:"block"
-          }}
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/footer/instagram.png?auto=format%2Ccompress&dpr=1&w=28&h=28&q=40"
-            alt=""
-            mr={4}
-          />
-          <Image
-          display={{
-            base:"none",
-            sm:"block"
-          }}
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera_assets.s3.amazonaws.com/images/9b7e964107839c77644d7e7d15035b73.png?auto=format%2Ccompress&dpr=1&w=28&h=28&q=40"
-            alt=""
-            mr={4}
-          />
+        {/* Social Links */}
+        <Flex gap={4} alignItems="center" justifyContent="center">
+
+          {/* LinkedIn */}
+          <Link
+            href="https://www.linkedin.com/in/suraj-kumar-42b5a1355/"
+            isExternal
+          >
+            <Image
+            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg"
+    alt="LinkedIn"
+    boxSize="28px" />
+          </Link>
+
+          {/* GitHub */}
+          <Link
+            href="https://github.com/Surajiitp"
+            isExternal
+          >
+            <Image
+              src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/github.svg"
+              alt="GitHub"
+              boxSize="28px"
+            />
+          </Link>
+
         </Flex>
       </Flex>
     </Flex>
--- frontend/src/Pages/LandingPageComponents/Section2.jsx
+++ frontend/src/Pages/LandingPageComponents/Section2.jsx
@@ -1,14 +1,17 @@
 import {
   Flex,
   Text,
-  Link,
   Grid,
-  Image,
   useBreakpointValue,
 } from "@chakra-ui/react";
 
 const Section2 = () => {
-  const columnCount = useBreakpointValue({ sm: 4, md: 6, lg: 8 });
+  const columnCount = useBreakpointValue({
+    base: 2,
+    sm: 2,
+    md: 4,
+    lg: 8,
+  });
 
   return (
     <Flex
@@ -16,78 +19,72 @@ const Section2 = () => {
       gap={6}
       bg="#f5f5f5"
       p={10}
-      justifyContent={"center"}
-      alignItems={"center"}
-    
+      justifyContent="center"
+      alignItems="center"
     >
       <Text
         fontSize={{
           lg: "2xl",
-          sm: "l",
+          sm: "lg",
           md: "xl",
         }}
+        textAlign="center"
       >
-        We collaborate with{" "}
-        <Link color="#0056d2" href="#" fontWeight="bold">
-          300+ leading universities and companies
-        </Link>
+        Discover courses designed to help you learn, grow, and succeed.
       </Text>
-      <Grid templateColumns={`repeat(${columnCount}, 1fr)`} gap={6}>
+
+      <Grid
+        templateColumns={`repeat(${columnCount}, 1fr)`}
+        gap={6}
+        width="100%"
+        maxW="1000px"
+      >
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/77hmeEJo3ZPlURCU02fD52/aa37b7f7b52285ba350acac62d8af5c1/illinois-3.png?auto=format%2Ccompress&dpr=1&w=&h=32"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Web Development
+          </Text>
         </Flex>
+
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/6XkOucZz6pMLV5DPvXCgCL/1777129a58b0a62b237bd28e9956afe8/duke-3.png?auto=format%2Ccompress&dpr=1&w=&h=32"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Data Science
+          </Text>
         </Flex>
+
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/1c6RjBHi3Lqb9QpWxje7iA/b529f909c5230af3210ba2d47d149620/google.png?auto=format%2Ccompress&dpr=1&w=&h=37"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Artificial Intelligence
+          </Text>
         </Flex>
+
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/60SA8pGxPXMmJf4n7umK1H/ccec31bbe2358210bf8391dcba6cd2f1/umich.png?auto=format%2Ccompress&dpr=1&w=&h=55"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Computer Science
+          </Text>
         </Flex>
+
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/3toC4I7jbWxiedfxiyNjtT/735faeaf976a9692f425f8c3a7d125dc/1000px-IBM_logo.svg.png?auto=format%2Ccompress&dpr=1&w=&h=32"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Programming
+          </Text>
         </Flex>
+
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/FHOd44z40jTFsSSao84AM/d1e357f5650a23bf2936114112d44445/imperial.png?auto=format%2Ccompress&dpr=1&w=&h=35"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Cloud Computing
+          </Text>
         </Flex>
+
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/4FSFmNXuDIzTvFb7n0v4mK/704ae9e0a7981fb6415f4cb4609bbbb3/stanford.svg?auto=format%2Ccompress&dpr=1&w=&h=27"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Cybersecurity
+          </Text>
         </Flex>
+
         <Flex justify="center" align="center" p={4}>
-          <Image
-            src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/1ZeiauXe5bPProvfuIo7o2/55d005d42979ab585cdfa01f825b7d4f/penn.svg?auto=format%2Ccompress&dpr=1&w=&h=37"
-            alt="Chakra Logo"
-            objectFit="contain"
-          />
+          <Text fontWeight="bold" textAlign="center">
+            Machine Learning
+          </Text>
         </Flex>
       </Grid>
     </Flex>
